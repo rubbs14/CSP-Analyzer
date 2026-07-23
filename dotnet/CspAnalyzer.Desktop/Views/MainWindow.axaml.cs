e8fc311ec508365250f9aafb2afabec7b01f0f26
rubbs14 <roberto.fino84@gmail.com>
2026-07-23 12:58:25 +0200

    S10b: wire charts, player nav, filter, and manual overrides into MainWindow

--- dotnet/CspAnalyzer.Desktop/Views/MainWindow.axaml.cs
+++ dotnet/CspAnalyzer.Desktop/Views/MainWindow.axaml.cs
@@ -1,4 +1,8 @@
 using Avalonia.Controls;
+using CspAnalyzer.Desktop.ViewModels;
+using LiveChartsCore.Kernel;
+using LiveChartsCore.Kernel.Sketches;
+using LiveChartsCore.SkiaSharpView.Avalonia;
 
 namespace CspAnalyzer.Desktop.Views;
 
@@ -7,5 +11,16 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        this.FindControl<CartesianChart>("PeakDiffChart")!.ChartPointPointerDown += OnChartPointClicked;
+        this.FindControl<CartesianChart>("ProbabilityChart")!.ChartPointPointerDown += OnChartPointClicked;
+    }
+
+    private void OnChartPointClicked(IChartView chart, ChartPoint point)
+    {
+        if (DataContext is MainViewModel vm)
+        {
+            vm.NavigateToChartIndex(point.Index);
+        }
     }
 }
